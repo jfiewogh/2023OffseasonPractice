c25fe30978c06649b4b5dbbc9a70fdc4bbe47dc9
frc2158 <frc2158@proton.me>
2023-11-01 11:10:43 -0700

    Add REVLib for Spark support; Clean up warnings

--- CommandRobotMinimal/src/main/java/frc/robot/commands/ExampleCommand.java
+++ CommandRobotMinimal/src/main/java/frc/robot/commands/ExampleCommand.java
@@ -9,7 +9,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
 public class ExampleCommand extends CommandBase {
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+
   private final ExampleSubsystem m_subsystem;
 
   /**
@@ -19,8 +19,9 @@ public class ExampleCommand extends CommandBase {
    */
   public ExampleCommand(ExampleSubsystem subsystem) {
     m_subsystem = subsystem;
+
     // Use addRequirements() here to declare subsystem dependencies.
-    addRequirements(subsystem);
+    addRequirements(m_subsystem);
   }
 
   // Called when the command is initially scheduled.
